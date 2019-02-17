6342f95aa12f03a48e390a5afcd39123157ba9fd
Noppartt <noparat.ra@ku.th>
2019-02-17 11:52:05 +0700

    'update'

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  
+formGroup :FormGroup;
+  constructor( private formBulid: FormBuilder ) 
+  {
+    
+  }
+
+  ngOnInit() {
+    this.formGroup = this.formBulid.group({
+      firstName: this.formBulid.control(''),
+      lastName:[''] 
+      
+    })
+  }
+  onSubmit(from: FormGroup){
+    console.log(from);
+  }
+}
